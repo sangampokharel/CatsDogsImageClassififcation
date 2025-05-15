7ca15502045e935dc338b98cb8dc50a91192eab1
sangam <sangampokharel803@gmail.com>
2025-05-15 13:23:56 +0545

    Initial Commit

---
+++ CatsDogsImageClassififcation/CatsDogsImageClassififcationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsDogsImageClassififcationApp.swift
+//  CatsDogsImageClassififcation
+//
+//  Created by EKbana on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct CatsDogsImageClassififcationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
